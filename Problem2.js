0b0ded9142aa722bff4fb0d2764c3eff32323189
Rana <76555387+HR-Rana@users.noreply.github.com>
2023-11-15 22:47:49 +0600

    Add files via upload

---
+++ Problem2.js
@@ -0,0 +1,44 @@
+
+
+
+const num = [1,2,3,4,5,6,7,8,9,10];
+
+function multiply(num){
+  num.forEach((i) => {
+  const result = ``
+     console.log(i,"*", 13 * i);
+   });
+}
+// multiply(num);
+
+
+function Lowercase(text){
+    console.log(text.toLowerCase())
+}
+
+// Lowercase("Hello js world..!");
+
+
+
+
+function Name(fName, lName){
+    const Fname = fName.concat(" ", lName);
+    console.log(Fname)
+};
+
+// Name("HR", "Rana");
+
+
+
+function Power(numb){
+   return console.log(numb*numb)
+};
+
+// Power(14)
+
+function Squqre(num){
+    const square =Math.pow(num,2);
+    console.log(square)
+}
+
+// Squqre(5)
