a1240aa7d51e4042043cd38a65c06caaa0234e34
Rana <76555387+HR-Rana@users.noreply.github.com>
2023-11-24 23:53:08 +0600

    Add files via upload

---
+++ 3rdDay/solve.js
@@ -0,0 +1,14 @@
+
+
+
+// ১. leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা চেক করো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন করবে।
+
+
+// function LeapYear(year){
+//     if(year %4 == 0){
+//         console.log("it's  a leapYear")
+//     }else{
+//         console.log("it not a Leap year...")
+//     }
+// }
+// LeapYear(2099);
